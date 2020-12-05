122b946fac0dea4fac39acaff6f6f8b316888312
rs66023 <rs66023@gmail.com>
2020-12-06 01:01:47 +0530

    Initial testing of Vue

--- search_engine/src/App.vue
+++ search_engine/src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="MyApplication">
     <h1>Hello World</h1>
+    Hello
     <p>little text behind the title</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
+#App {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
