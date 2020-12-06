31e3ad5b6eb5e1d48fa1478375d57ab628f4063d
rs66023 <rs66023@gmail.com>
2020-12-06 11:33:11 +0530

    Starting over on Vue 2

--- search_engine/src/main.js
+++
@@ -1,6 +0,0 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
